3ef3ab925d67ee5070dc8f3e9140b7769d5fddd6
yogeshAgarwal <yogeshagarwal1997199@gmail.com>
2018-10-22 22:27:00 +0530

    add comment and other stuffs

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, PostAdmin, CommentAdmin
+# from adminfilters.models import Post
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin )
+
+# class PostAdmin(admin.ModelAdmin):
+#     list_display =('title', 'published_date')
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,12 @@ urlpatterns = [
     path('post/<int:pk>/',views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    url('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+
+
 ]
